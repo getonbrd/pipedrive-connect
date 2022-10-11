40b87fd77b946242184df500cdcaf662e8596969
Jorge Rodriguez <jorge.rodriguez.suarez@gmail.com>
2022-10-11 11:35:16 -0400

    Return meaningful error with contextual data

--- lib/pipedrive/errors.rb
+++ lib/pipedrive/errors.rb
@@ -2,12 +2,13 @@
 
 module Pipedrive
   class PipedriveError < StandardError
-    attr_reader :code
+    attr_reader :code, :data
 
-    def initialize(message = nil, code = nil)
+    def initialize(message = nil, code = nil, data = nil)
       super(message)
       @message = message
       @code = code
+      @data = data
     end
 
     def message
@@ -15,8 +16,60 @@ module Pipedrive
       "#{code_str}#{@message}"
     end
   end
+
+  # As documented at https://pipedrive.readme.io/docs/core-api-concepts-http-status-codes
   class SettingError < PipedriveError; end
   class AuthenticationError < PipedriveError; end
   class NotFoundError < PipedriveError; end
+  class BadRequestError < PipedriveError; end
+  class UnauthorizedError < PipedriveError; end
+  class PaymentRequiredError < PipedriveError; end
+  class ForbiddenError < PipedriveError; end
+  class MethodNotAllowedError < PipedriveError; end
+  class GoneError < PipedriveError; end
+  class UnsupportedMediaTypeError < PipedriveError; end
+  class UnprocessableEntityError < PipedriveError; end
+  class TooManyRequestsError < PipedriveError; end
+  class InternalServerError < PipedriveError; end
+  class NotImplementedError < PipedriveError; end
+  class ServiceUnavailableError < PipedriveError; end
   class UnkownAPIError < PipedriveError; end
+
+  ERROR_CLASS_MAP = {
+    "400" => BadRequestError,
+    "401" => UnauthorizedError,
+    "402" => PaymentRequiredError,
+    "403" => ForbiddenError,
+    "404" => NotFoundError,
+    "405" => MethodNotAllowedError,
+    "410" => GoneError,
+    "415" => UnsupportedMediaTypeError,
+    "422" => UnprocessableEntityError,
+    "429" => TooManyRequestsError,
+    "500" => InternalServerError,
+    "501" => NotImplementedError,
+    "503" => ServiceUnavailableError,
+  }.freeze
+
+  def raise_error(status, response)
+    return if [200, 201].include?(status)
+
+    message =
+      [response.dig(:error), response.dig(:error_info)]
+      .compact
+      .join(". ")
+
+    error_data =
+      response
+      .fetch(:data, {})
+      .inspect
+      .concat(response.fetch(:additional_data, {}).inspect)
+    error_class = ERROR_CLASS_MAP[status.to_s]
+
+    raise error_class.new(message, status, error_data) if error_class
+
+    raise UnkownAPIError.new(message, status, error_data)
+  end
+
+  module_function :raise_error
 end
---
+++ spec/lib/pipedrive/pipedrive_spec.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Pipedrive do
+  describe "#raise_error" do
+    shared_examples_for "error" do |code, klass, context|
+      let(:message) do
+        [context.dig(:error), context.dig(:error_info)]
+          .compact
+          .join(". ")
+      end
+
+      it "raises error" do
+        expect do
+          described_class.raise_error(code, context)
+        end.to raise_error(klass, /#{message}/)
+      end
+    end
+
+    describe "error codes" do
+      it_behaves_like "error",
+                      400,
+                      Pipedrive::BadRequestError,
+                      error: "Bad Request"
+
+      it_behaves_like "error",
+                      401,
+                      Pipedrive::UnauthorizedError,
+                      error: "Invalid API token"
+
+      it_behaves_like "error",
+                      402,
+                      Pipedrive::PaymentRequiredError,
+                      error: "Payment Required",
+                      error_info: "Company account is not open"
+
+      it_behaves_like "error",
+                      403,
+                      Pipedrive::ForbiddenError,
+                      error: "Forbidden",
+                      error_info: "Request not allowed."
+
+      it_behaves_like "error",
+                      404,
+                      Pipedrive::NotFoundError,
+                      error: "Not Found",
+                      error_info: "Resource unavailable"
+
+      it_behaves_like "error",
+                      405,
+                      Pipedrive::MethodNotAllowedError,
+                      error: "Method not allowed",
+                      error_info: "Incorrect request method"
+
+      it_behaves_like "error",
+                      410,
+                      Pipedrive::GoneError,
+                      error: "Gone",
+                      error_info: "Old resource permanently unavailable"
+
+      it_behaves_like "error",
+                      415,
+                      Pipedrive::UnsupportedMediaTypeError,
+                      error: "Unsupported Media Type",
+                      error_info: "Feature is not enabled"
+
+      it_behaves_like "error",
+                      422,
+                      Pipedrive::UnprocessableEntityError,
+                      error: "Unprocessable entity",
+                      error_info: "Webhooks limit reached"
+
+      it_behaves_like "error",
+                      429,
+                      Pipedrive::TooManyRequestsError,
+                      error: "Too Many Requests",
+                      error_info: "Rate limit has been exceeded"
+
+      it_behaves_like "error",
+                      500,
+                      Pipedrive::InternalServerError,
+                      error: "Internal Server Error",
+                      error_info: "Generic server error"
+
+      it_behaves_like "error",
+                      501,
+                      Pipedrive::NotImplementedError,
+                      error: "Not Implemented",
+                      error_info: "Non-existent functionality"
+
+      it_behaves_like "error",
+                      503,
+                      Pipedrive::ServiceUnavailableError,
+                      error: "Service Unavailable",
+                      error_info: "Scheduled maintenance"
+
+      it_behaves_like "error",
+                      1000,
+                      Pipedrive::UnkownAPIError,
+                      error: "Unknown error",
+                      error_info: "Error not mapped"
+    end
+
+    describe "error data" do
+      let(:response) do
+        {
+          error: "Bad request",
+          data: "Error data",
+          additional_data: { abc: 123 },
+        }
+      end
+
+      it "includes data and additional data into the error" do
+        described_class.raise_error(1000, response)
+      rescue StandardError => e
+        expect(e.data).to eq "\"Error data\"{:abc=>123}"
+      end
+    end
+  end
+end
