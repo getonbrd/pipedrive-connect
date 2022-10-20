a835bf41a751ecbd5ba3777f88acdfa8b668e8d9
Jorge Rodriguez <jorge.rodriguez.suarez@gmail.com>
2022-10-20 08:16:52 -0400

    Better if sentences

--- lib/pipedrive/util.rb
+++ lib/pipedrive/util.rb
@@ -3,13 +3,13 @@
 module Pipedrive
   module Util
     def self.serialize_response(response, symbolize_names: true)
-      if Pipedrive.treat_no_content_as_not_found && response.status == 204
+      if response.status == 204
+        return {} unless Pipedrive.treat_no_content_as_not_found
+
         Pipedrive.raise_error(404, error: "HTTP 204 status code received. No content")
       end
 
       if response.success?
-        return {} if response.status == 204
-
         JSON.parse(response.body, symbolize_names: symbolize_names)
       else
         error_body = JSON.parse(response.body, symbolize_names: symbolize_names)
