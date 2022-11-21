14a21772a983677e5d021a9cb400faa83476f757
Matthieu Rondeau <matt@matthieurondeau.fr>
2022-11-21 11:44:01 +0100

    fix(users): fix search to use correct URL

--- lib/pipedrive/resources/user.rb
+++ lib/pipedrive/resources/user.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
 module Pipedrive
-  class User < Resource; end
+  class User < Resource
+    def self.search(term, search_by_email)
+      params = { term: term }
+      params = params.merge({ search_by_email: 1 }) if search_by_email
+      response = request(
+        :get,
+        "#{resource_url}/find",
+        params
+      )
+      items = response[:data]
+
+      return [] if items.nil?
+
+      items.map { |d| new(d) }
+    end
+  end
 end
