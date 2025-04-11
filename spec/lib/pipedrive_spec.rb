3506f266203212e171fb5ec42aa2906aae230b3b
Nelson Pascoal <rug.unlucky920@mailer.me>
2025-04-11 11:23:26 +0200

    feat: new specs foir versioning logic

--- spec/lib/pipedrive_spec.rb
+++ spec/lib/pipedrive_spec.rb
@@ -19,4 +19,32 @@ RSpec.describe Pipedrive do
       expect(Pipedrive.logger).to eq(new_logger)
     end
   end
+
+  describe "#api_version" do
+    context "when default version is used" do
+      it "returns v2" do
+        expect(described_class.api_version).to eq(:v2)
+      end
+    end
+
+    context "when set to v1" do
+      before do
+        described_class.use_v1_api!
+      end
+
+      it "returns v1" do
+        expect(described_class.api_version).to eq(:v1)
+      end
+    end
+
+    context "when set to v2" do
+      before do
+        described_class.use_v2_api!
+      end
+
+      it "returns v2" do
+        expect(described_class.api_version).to eq(:v2)
+      end
+    end
+  end
 end
